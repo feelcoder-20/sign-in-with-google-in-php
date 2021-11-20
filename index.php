701201852661a1a7a0edcfefe1fe61e611c3838d
feelcoder-20 <73095378+feelcoder-20@users.noreply.github.com>
2021-11-20 16:59:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signin with Google</title>
     <script src="http://apis.google.com/js/platform.js?onload=initAuth" async defer></script>
-    <meta name="google-signin-client_id" content="">
+    <meta name="google-signin-client_id" content="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX">
     <script src="http://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <style>
    * {
